bd100208100e2ffbfff01ae484cef774b7540595
Lucia <lulipalazzini@gmail.com>
2025-05-23 16:42:33 -0300

    works&projects sketch

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,35 @@
 import "./Projects.css";
+import projects from "../../data/projects.json";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const renderProjectCards = projects.map((project) => (
+    <div key={project.id} className="card">
+      <img
+        src={project.imageUrl}
+        alt={project.projectName}
+        className="cardImage"
+      />
+      <div>
+        <h3 className="cardName">{project.projectName}</h3>
+        <p className="cardDesc">{project.projectDescription}</p>
+        <a
+          href={project.projectLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cardLink"
+        >
+          Ver proyecto
+        </a>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="projects">
+      <h1 className="title"># Works && Projects</h1>
+      <div className="projectContainer">{renderProjectCards}</div>
+    </div>
+  );
 };
 
 export default Projects;
